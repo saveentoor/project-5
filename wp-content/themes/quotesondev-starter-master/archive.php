c9df7e99d948ba2fc2c4dd7dc59573747e23cb50
Saveen Toor <toorsaveen@gmail.com>
2019-02-28 15:06:49 -0800

    The Archives page now has categories, authors and tags on it.

--- wp-content/themes/quotesondev-starter-master/archive.php
+++ wp-content/themes/quotesondev-starter-master/archive.php
@@ -9,34 +9,33 @@
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
-<h1>Archives:</h1>
-<h2>Quote Authors</h2>
 <?php
-        
         $the_query = new WP_Query(array(
             'post_type'      => 'post',
             'orderby'        => 'title',
             'order'          => 'ASC',
             'posts_per_page' => -1,
-        )); ?>
-        <div class="list-all-authors">
-            <?php  /* Start the Loop */ ?>
-            <?php while ($the_query->have_posts()): $the_query->the_post();  ?>
-            <?php the_title(sprintf('<span class="each-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></span>'); ?>
-            <?php endwhile;
+		)); ?>
+		
+<div class="list-all-authors">
+
+	 <h1>Archives:</h1>
+	 <h2>Quote Authors</h2>
+        <?php while ($the_query->have_posts()): $the_query->the_post();  ?>
+        <?php the_title(sprintf('<span class="each-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></span>'); ?>            <?php endwhile;
         wp_reset_postdata(); ?>
-		</div>
+</div>
 		
-		<div class="list-all-categories">
-		<h2>Categories</h2>
-		 
+<div class="list-all-categories">
+	<h2>Categories</h2>
+	<?php wp_list_categories(array()); ?>
+</div>
 
-		<?php wp_list_categories(array()); ?>
+<div class="list-all-tags">
+	<h2>Tags</h2>
+	<?php wp_tag_cloud(array('tags')); ?>
+</div>
 
-		</div>
-
-		<h2>Tags</h2>
-		
 		<?php endwhile; // End of the loop. ?>
 
 </main><!-- #main -->
