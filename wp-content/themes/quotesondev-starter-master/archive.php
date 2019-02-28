9952610d62d013b0ba1a399d8d6e8e1be63a1214
Saveen Toor <toorsaveen@gmail.com>
2019-02-28 14:32:35 -0800

    On the archives page, all the categories are now appearing.

--- wp-content/themes/quotesondev-starter-master/archive.php
+++ wp-content/themes/quotesondev-starter-master/archive.php
@@ -7,22 +7,33 @@
  */
 
 get_header(); ?>
-
-<h2>dfd</h2>
+<h1>Archives:</h1>
+<h2>Quote Authors</h2>
 <?php
-
+        
         $the_query = new WP_Query(array(
             'post_type'      => 'post',
             'orderby'        => 'title',
             'order'          => 'ASC',
             'posts_per_page' => -1,
         )); ?>
-        <div class="all-authors">
+        <div class="list-all-authors">
             <?php  /* Start the Loop */ ?>
             <?php while ($the_query->have_posts()): $the_query->the_post();  ?>
             <?php the_title(sprintf('<span class="each-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></span>'); ?>
             <?php endwhile;
         wp_reset_postdata(); ?>
+		</div>
+		
+		<div class="list-all-categories">
+		<h2>Categories</h2>
+		 
+
+		<?php wp_list_categories(array()); ?>
+
+		</div>
+
+		<h2>Tags</h2>
 		
 
 	
